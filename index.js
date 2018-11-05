4fef59c3a2dd08d32071456fb1c52b8447da7e0a
SAURABH <sameer.saurabh@capgemini.com>
2018-11-05 14:35:45 +0530

    added Google Custom Search Engine Api

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var AWS = require('aws-sdk');
 AWS.config.update({
     region: 'us-east-1'
 });
+
+
 var rekognition = new AWS.Rekognition();
+
+require('dotenv').config();
+var imageSearch = require('node-google-image-search');
 
 exports.handler = function (event,context,callback)  {
    
@@ -13,7 +18,7 @@ exports.handler = function (event,context,callback)  {
     Image: { /* required */
       S3Object: {
         Bucket: 'celeb-lamda-function',
-        Name: 'bijaya.jpg'
+        Name: 'saurav.jpg'
       }
     }
   };
@@ -26,41 +31,59 @@ exports.handler = function (event,context,callback)  {
             if (data.CelebrityFaces.length == 0) {
                 console.log('---------Celebrity Not Recognized..........');
                 
-                sightEngineApi(imgName);
-                callback(null,'Not Recognized');
+                sightEngineApi();
+                
             }
             else {
                 
-                console.log(data); // successful response
-                callback(null,data);
-                console.log('-----------------------End--------------------------------');
+                response.celebName=data.CelebrityFaces[0].Name;
+                
+                response.probWithUser= data.CelebrityFaces[0].MatchConfidence;
+                
+                imageSearch(response.celebName, result, 0, 1);
+             
             }
         }
     });
 
-
-function sightEngineApi(imgName) {
+var response={
+    celebName:"",
+    probWithUser:"",
+    celebImageLink:""
+};
+ 
+function sightEngineApi() {
     console.log('-------------In Sight Engine----------------');
 
-    var sightengine = require('sightengine')("", "");
+    var sightengine = require('sightengine')("1538590156", "LiKUMaTzuNe2XAXbk66E");
 
     sightengine.check(['celebrities']).set_url('https://s3.amazonaws.com/celeb-lamda-function/bijaya.jpg').then(function (result) {
-        // read the output (result)
-        console.log('Printing Result');
-       console.log(result);
-       callback(null,result);
-        // console.log("features   : " + result.faces[0].features);
-        // console.log("celebrity name : " + result.faces[0].celebrity[0].name);
-        // console.log("celebrity prob : " + result.faces[0].celebrity[0].prob);
-        // console.log("celebrity name : " + result.faces[0].celebrity[1].name);
-        // console.log("celebrity prob : " + result.faces[0].celebrity[1].prob);
-        // console.log('-----------------------End--------------------------------');
+        
+        response.celebName=result.faces[0].celebrity[0].name;
+        response.probWithUser=result.faces[0].celebrity[0].prob;
+        
+        console.log("celebrity name : " +  response.celebName);
+        console.log("celebrity prob : " + response.probWithUser);
+        
+        imageSearch(response.celebName, result, 0, 1);
+        
     }).catch(function (err) {
         // handle the error
         console.log(err.stack);
-        console.log(null,err);
+         callback(null,err);
     });
+}
 
+function result(results){
+      
+        response.celebImageLink=results[0].link;
+        
+        console.log("response :");
+        console.log(response);
+         
+        callback(null,response);
+        
+        console.log('-----------------------End--------------------------------');
 }
 
 };
